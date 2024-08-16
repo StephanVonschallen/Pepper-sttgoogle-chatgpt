339aa012037841a27d389d5203d26b2b5cc900c2
Stephan Vonschallen <stephan.vonschallen@fhnw.ch>
2024-08-16 13:29:13 +0200

    exe opens the two programms in different consoles

--- exe.py
+++ exe.py
@@ -1,28 +1,31 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
+import os
+import time
 
 # Define the strings shared between the scripts
-pip_address = "169.254.126.93"
+pip_address = "169.254.83.74"
 python_executable = "c:/Python27/python.exe"
 
 # Define the paths to the scripts
 script_path_1 = "c:/Users/ma1177259/GitRepo/sttgoogle-chatgpt/pepperspeechrecognition/module_speechrecognition.py"
 script_path_2 = "c:/Users/ma1177259/GitRepo/sttgoogle-chatgpt/peppergpt_module_saveData.py"
 
-# Construct the commands to be executed
-command_1 = [python_executable, script_path_1, "--pip", pip_address]
-command_2 = [python_executable, script_path_2, "--pip", pip_address]
+# Function to open a new command window and run a command
+def run_in_new_console(command):
+    cmd = 'start cmd /k "{}"'.format(command)
+    subprocess.Popen(cmd, shell=True)
 
-# Execute the commands in separate consoles (using subprocess.Popen)
-process_1 = subprocess.Popen(command_1, shell=True)
-process_2 = subprocess.Popen(command_2, shell=True)
+# Construct the commands to be executed in new command windows
+command_1 = '{} "{}" --pip {}'.format(python_executable, script_path_1, pip_address)
+command_2 = '{} "{}" --pip {}'.format(python_executable, script_path_2, pip_address)
 
-# Wait for both processes to complete
-while process_1.poll() is None or process_2.poll() is None:
-    # Poll() returns None if the process is still running
-    # This loop will continue until both processes are done
-    continue
+# Run both commands in separate command windows
+run_in_new_console(command_1)
 
-# Both processes are now finished
-print("Both processes have completed.")
+time.sleep(4)
+
+run_in_new_console(command_2)
+
+print("Both processes have been started in separate command windows.")
